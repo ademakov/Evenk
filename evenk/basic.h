e7ac5f24cdeb8679cdf0b47a1239f4633fd66e1a
Aleksey Demakov <ademakov@gmail.com>
2015-04-23 03:09:32 +0600

    Add a header file for basic definitions.

---
+++ evenk/basic.h
@@ -0,0 +1,14 @@
+// Basic definitions.
+
+#ifndef EVENK_BASIC_H_
+#define EVENK_BASIC_H_
+
+#include <cstddef>
+
+namespace ev {
+
+constexpr std::size_t kCacheLineSize = 64;
+
+}
+
+#endif  // !EVENK_BASIC_H_
