6a54e73a7e48ba6dc79a7c0d2afec57f801837a4
Aleksey Demakov <ademakov@gmail.com>
2015-04-23 03:19:05 +0600

    Add concurrency bencharks.

---
+++ tests/concurrency-lock-bench.cc
@@ -0,0 +1,128 @@
+#include "evenk/concurrency.h"
+
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <vector>
+
+namespace evc = ev::concurrency;
+
+evc::StdMutex mutex;
+evc::PosixMutex posix_mutex;
+evc::SpinLock spin_lock;
+evc::TicketLock ticket_lock;
+evc::FutexLock futex_lock;
+
+evc::NoBackoff no_backoff;
+evc::YieldBackoff yield_backoff;
+
+evc::LinearBackoff<evc::CPUCycle> linear_cycle_backoff(40);
+evc::ExponentialBackoff<evc::CPUCycle> exponential_cycle_backoff(40);
+evc::ProportionalBackoff<evc::CPUCycle> proportional_cycle_backoff(40);
+
+evc::LinearBackoff<evc::CPURelax> linear_relax_backoff(5);
+evc::ExponentialBackoff<evc::CPURelax> exponential_relax_backoff(5);
+evc::ProportionalBackoff<evc::CPURelax> proportional_relax_backoff(5);
+
+evc::LinearBackoff<evc::NanoSleep> linear_sleep_backoff(10);
+evc::ExponentialBackoff<evc::NanoSleep> exponential_sleep_backoff(10);
+evc::ProportionalBackoff<evc::NanoSleep> proportional_sleep_backoff(10);
+
+evc::CompositeBackoff<evc::LinearBackoff<evc::CPUCycle>, evc::YieldBackoff>
+    cycle_yield_backoff(linear_cycle_backoff, yield_backoff);
+evc::CompositeBackoff<evc::LinearBackoff<evc::CPURelax>, evc::YieldBackoff>
+    relax_yield_backoff(linear_relax_backoff, yield_backoff);
+
+template <typename Lock, typename... Backoff>
+void spin(int &count, Lock &lock, Backoff... backoff) {
+  for (int i = 0; i < 100 * 1000; ++i) {
+    lock.Lock(backoff...);
+    evc::CPUCycle{}(1000);
+    ++count;
+    lock.Unlock();
+    evc::CPUCycle{}(5000);
+  }
+}
+
+template <typename Lock, typename... Backoff>
+void bench(unsigned nthreads, std::string const &name, Lock &lock,
+           Backoff... backoff) {
+  int count = 0;
+
+  std::vector<std::thread> v;
+  v.reserve(nthreads);
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (unsigned i = 0; i < nthreads; ++i)
+    v.emplace_back(spin<Lock, Backoff...>, std::ref(count), std::ref(lock),
+                   backoff...);
+  for (auto &t : v)
+    t.join();
+
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> diff = end - start;
+
+  std::cout << name << ": count=" << count << ", duration=" << diff.count()
+            << "\n";
+}
+
+void bench(unsigned nthreads) {
+  std::cout << "Threads: " << nthreads << "\n";
+
+#define BENCH1(lock) bench(nthreads, #lock, lock)
+#define BENCH2(lock, backoff) bench(nthreads, #lock " " #backoff, lock, backoff)
+
+  BENCH1(mutex);
+  BENCH1(posix_mutex);
+
+#if __linux__
+  BENCH2(futex_lock, no_backoff);
+#endif
+
+#if __linux__
+  BENCH2(futex_lock, linear_cycle_backoff);
+  BENCH2(futex_lock, exponential_cycle_backoff);
+  BENCH2(futex_lock, proportional_cycle_backoff);
+  BENCH2(futex_lock, linear_relax_backoff);
+  BENCH2(futex_lock, exponential_relax_backoff);
+  BENCH2(futex_lock, proportional_relax_backoff);
+#endif
+
+  BENCH2(spin_lock, no_backoff);
+  BENCH2(spin_lock, linear_cycle_backoff);
+  BENCH2(spin_lock, exponential_cycle_backoff);
+  BENCH2(spin_lock, proportional_cycle_backoff);
+  BENCH2(spin_lock, linear_relax_backoff);
+  BENCH2(spin_lock, exponential_relax_backoff);
+  BENCH2(spin_lock, proportional_relax_backoff);
+  BENCH2(spin_lock, yield_backoff);
+  BENCH2(spin_lock, cycle_yield_backoff);
+  BENCH2(spin_lock, relax_yield_backoff);
+  BENCH2(spin_lock, linear_sleep_backoff);
+  BENCH2(spin_lock, exponential_sleep_backoff);
+  BENCH2(spin_lock, proportional_sleep_backoff);
+
+  BENCH2(ticket_lock, no_backoff);
+  BENCH2(ticket_lock, linear_cycle_backoff);
+  BENCH2(ticket_lock, exponential_cycle_backoff);
+  BENCH2(ticket_lock, proportional_cycle_backoff);
+  BENCH2(ticket_lock, linear_relax_backoff);
+  BENCH2(ticket_lock, exponential_relax_backoff);
+  BENCH2(ticket_lock, proportional_relax_backoff);
+  BENCH2(ticket_lock, yield_backoff);
+  BENCH2(ticket_lock, cycle_yield_backoff);
+  BENCH2(ticket_lock, relax_yield_backoff);
+  // BENCH2(ticket_lock, linear_sleep_backoff);
+  // BENCH2(ticket_lock, exponential_sleep_backoff);
+  // BENCH2(ticket_lock, proportional_sleep_backoff);
+
+  std::cout << "\n";
+}
+
+int main() {
+  unsigned n = std::thread::hardware_concurrency();
+  for (unsigned i = 1; i <= n; i += i)
+    bench(i);
+  return 0;
+}
---
+++ tests/concurrency-queue-bench.cc
@@ -0,0 +1,181 @@
+#include "evenk/concurrency.h"
+
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <cstring>
+#include <vector>
+
+namespace evc = ev::concurrency;
+
+template <typename Queue, typename... Backoff>
+void consume(Queue& queue, size_t& count, Backoff... backoff) {
+  std::string data;
+  while (queue.Dequeue(data, backoff...)) {
+    ++count;
+  }
+}
+
+template <typename Queue, typename... Backoff>
+void produce(Queue& queue, int count, Backoff... backoff) {
+  std::string data = "this is a test string";
+  for (int i = 0; i < count; i++) {
+    queue.Enqueue(data, backoff...);
+  }
+}
+
+template <typename Queue, typename... Backoff>
+void bench(unsigned nthreads, const std::string& name, Queue& queue,
+           Backoff... backoff) {
+  std::vector<size_t> counts(nthreads);
+  std::vector<std::thread> threads(nthreads);
+
+  auto start = std::chrono::steady_clock::now();
+
+  for (size_t i = 0; i < threads.size(); i++)
+    threads[i] = std::thread(consume<Queue, Backoff...>, std::ref(queue),
+                             std::ref(counts[i]), backoff...);
+
+#define TOTAL (250 * 1000)
+
+  produce(queue, TOTAL, backoff...);
+  queue.Finish();
+
+  for (auto& t : threads) t.join();
+
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> diff = end - start;
+
+  size_t total = 0;
+  for (auto& c : counts) total += c;
+
+  std::cout << name << ": duration=" << diff.count() << ", count=" << total
+            << "\n";
+  if (total != TOTAL) std::cout << "FAIL!!!\n";
+
+  for (auto& c : counts) std::cout << " " << c;
+  std::cout << '\n';
+}
+
+void bench(unsigned nthreads) {
+  std::cout << "Threads: " << nthreads << "\n";
+
+#define BENCH1(queue) bench(nthreads, #queue, queue)
+#define BENCH2(queue, backoff) \
+  bench(nthreads, #queue " " #backoff, queue, backoff)
+
+  evc::Queue<std::string, evc::StdSynch> std_queue;
+  evc::Queue<std::string, evc::PosixSynch> posix_queue;
+
+  BENCH1(std_queue);
+  BENCH1(posix_queue);
+
+#if __linux__
+  {
+    evc::Queue<std::string, evc::FutexSynch> futex_queue;
+    BENCH1(futex_queue);
+  }
+  {
+    evc::Queue<std::string, evc::FutexSynch> futex_queue;
+    evc::LinearBackoff<evc::CPUCycle> linear_cycle_backoff(100000);
+    BENCH2(futex_queue, linear_cycle_backoff);
+  }
+  {
+    evc::Queue<std::string, evc::FutexSynch> futex_queue;
+    evc::LinearBackoff<evc::CPURelax> linear_relax_backoff(100000);
+    BENCH2(futex_queue, linear_relax_backoff);
+  }
+  {
+    evc::Queue<std::string, evc::FutexSynch> futex_queue;
+    evc::YieldBackoff yield_backoff;
+    BENCH2(futex_queue, yield_backoff);
+  }
+#endif
+
+  evc::BoundedQueue<std::string> bounded_queue(1024);
+  BENCH1(bounded_queue);
+
+  evc::BoundedQueue<std::string, evc::BoundedQueueSynchWait<evc::StdSynch>>
+      bounded_std_synch_queue(1024);
+  BENCH1(bounded_std_synch_queue);
+
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueSynchWait<evc::StdSynch>>
+        bounded_std_synch_queue(1024);
+    evc::LinearBackoff<evc::CPUCycle> linear_cycle_backoff(100000);
+    BENCH2(bounded_std_synch_queue, linear_cycle_backoff);
+  }
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueSynchWait<evc::StdSynch>>
+        bounded_std_synch_queue(1024);
+    evc::LinearBackoff<evc::CPURelax> linear_relax_backoff(100000);
+    BENCH2(bounded_std_synch_queue, linear_relax_backoff);
+  }
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueSynchWait<evc::StdSynch>>
+        bounded_std_synch_queue(1024);
+    evc::YieldBackoff yield_backoff;
+    BENCH2(bounded_std_synch_queue, yield_backoff);
+  }
+
+#if __linux__
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueSynchWait<evc::FutexSynch>>
+        bounded_futex_synch_queue(1024);
+    BENCH1(bounded_futex_synch_queue);
+  }
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueSynchWait<evc::FutexSynch>>
+        bounded_futex_synch_queue(1024);
+    evc::LinearBackoff<evc::CPUCycle> linear_cycle_backoff(100000);
+    BENCH2(bounded_futex_synch_queue, linear_cycle_backoff);
+  }
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueSynchWait<evc::FutexSynch>>
+        bounded_futex_synch_queue(1024);
+    evc::LinearBackoff<evc::CPURelax> linear_relax_backoff(100000);
+    BENCH2(bounded_futex_synch_queue, linear_relax_backoff);
+  }
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueSynchWait<evc::FutexSynch>>
+        bounded_futex_synch_queue(1024);
+    evc::YieldBackoff yield_backoff;
+    BENCH2(bounded_futex_synch_queue, yield_backoff);
+  }
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueFutexWait>
+        bounded_futex_queue(1024);
+    BENCH1(bounded_futex_queue);
+  }
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueFutexWait>
+        bounded_futex_queue(1024);
+    evc::LinearBackoff<evc::CPUCycle> linear_cycle_backoff(100000);
+    BENCH2(bounded_futex_queue, linear_cycle_backoff);
+  }
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueFutexWait>
+        bounded_futex_queue(1024);
+    evc::LinearBackoff<evc::CPURelax> linear_relax_backoff(100000);
+    BENCH2(bounded_futex_queue, linear_relax_backoff);
+  }
+  {
+    evc::BoundedQueue<std::string, evc::BoundedQueueFutexWait>
+        bounded_futex_queue(1024);
+    evc::YieldBackoff yield_backoff;
+    BENCH2(bounded_futex_queue, yield_backoff);
+  }
+#endif
+
+  evc::BoundedQueue<std::string, evc::BoundedQueueYieldWait>
+      bounded_yield_queue(1024);
+  BENCH1(bounded_yield_queue);
+
+  std::cout << "\n";
+}
+
+int main() {
+  unsigned n = std::thread::hardware_concurrency();
+  for (unsigned i = 1; i <= n; i += i) bench(i);
+  return 0;
+}
