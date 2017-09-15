8fce7b47569e00a8b58a25ee575c6fcc0c4df32e
Aleksey Demakov <ademakov@gmail.com>
2017-09-15 11:03:01 +0700

    Update copyright

--- evenk/synch_queue.h
+++ evenk/synch_queue.h
@@ -1,7 +1,7 @@
 //
 // Simple Concurrent Queue
 //
-// Copyright (c) 2015-2016  Aleksey Demakov
+// Copyright (c) 2015-2017  Aleksey Demakov
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
