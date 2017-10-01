d087a49bdea8ce00fee69aa901ad6917faa88719
Aleksey Demakov <ademakov@gmail.com>
2017-10-01 23:44:46 +0700

    Extend thread test

--- tests/thread-test.cc
+++ tests/thread-test.cc
@@ -1,5 +1,5 @@
-#include <evenk/thread.h>
 #include <evenk/synch.h>
+#include <evenk/thread.h>
 
 #include <iostream>
 
@@ -7,28 +7,45 @@ evenk::default_synch::lock_type lock;
 evenk::default_synch::cond_var_type cond;
 
 void
-test()
+thread_routine()
 {
 	evenk::default_synch::lock_owner_type guard(lock);
-	cond.notify_one();
-	cond.wait(guard);
+	cond.notify_one(); // notify that the thread started
+	cond.wait(guard);  // wait for test finish
+}
+
+void
+print_affinity(const evenk::thread::cpuset_type &affinity)
+{
+	for (std::size_t cpu = 0; cpu < affinity.size(); cpu++)
+		std::cout << cpu << " ";
+	std::cout << "\n";
 }
 
 int
 main()
 {
 	evenk::default_synch::lock_owner_type guard(lock);
-	evenk::thread thread(test);
+	evenk::thread thread(thread_routine);
 	cond.wait(guard); // wait until the thread starts
 	guard.unlock();
 
-	auto affinity = thread.affinity();
-	for (std::size_t cpu = 0; cpu < affinity.size(); cpu++)
-		std::cout << cpu << " ";
-	std::cout << "\n";
+	{
+		auto affinity = thread.affinity();
+		print_affinity(affinity);
+
+		for (std::size_t i = 0; i < affinity.size(); i++)
+			affinity[i] = false;
+		thread.affinity(affinity);
+	}
+
+	{
+		auto affinity = thread.affinity();
+		print_affinity(affinity);
+	}
 
 	guard.lock();
-	cond.notify_one();
+	cond.notify_one(); // notify about test finish
 	guard.unlock();
 
 	thread.join();
