b24f496c213512ff365fc6cf6b6214bab8f39bda
Aleksey Demakov <ademakov@gmail.com>
2019-05-16 21:58:46 +0300

    Add output to the thread test

--- tests/thread-test.cc
+++ tests/thread-test.cc
@@ -10,17 +10,25 @@ void
 thread_routine()
 {
 	evenk::default_synch::lock_owner_type guard(lock);
+	std::cout << "The created thread notifies the main thread.\n";
 	cond.notify_one(); // notify that the thread started
+	std::cout << "The created thread waits for a notification from the main thread.\n";
 	cond.wait(guard);  // wait for test finish
+	std::cout << "The created thread gets a notification and exits.\n";
 }
 
 void
 print_affinity(const evenk::thread::cpuset_type &affinity)
 {
-	std::cout << affinity.size() << ":";
-	for (std::size_t cpu = 0; cpu < affinity.size(); cpu++)
-		if (affinity[cpu])
-			std::cout << " " << cpu;
+	std::cout << "CPU affinity info ";
+	if (affinity.size() == 0) {
+		std::cout << "is not available";
+	} else {
+		std::cout << ':' << affinity.size() << " CPUs:";
+		for (std::size_t cpu = 0; cpu < affinity.size(); cpu++)
+			if (affinity[cpu])
+				std::cout << " " << cpu;
+	}
 	std::cout << "\n";
 }
 
@@ -28,8 +36,10 @@ int
 main()
 {
 	evenk::default_synch::lock_owner_type guard(lock);
+	std::cout << "The main thread creates a new thread and waits for a notification from it.\n";
 	evenk::thread thread(thread_routine);
 	cond.wait(guard); // wait until the thread starts
+	std::cout << "The main thread gets a notification.\n";
 	guard.unlock();
 
 	{
@@ -47,10 +57,12 @@ main()
 	}
 
 	guard.lock();
+	std::cout << "The main thread notifies the created thread.\n";
 	cond.notify_one(); // notify about test finish
 	guard.unlock();
 
 	thread.join();
+	std::cout << "The main thread joins with the created thread and exits.\n";
 
 	return 0;
 }
